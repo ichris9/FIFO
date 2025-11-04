0d8846845d4bf89278667e3d7e2fc0c49a68be8d
ichris9 <christian.h2k6@gmail.com>
2025-11-03 21:29:52 -0300

    Update UsuarioDAO.java
    terminei de fazer o delete, insert.

--- src/fifo/UsuarioDAO.java
+++ src/fifo/UsuarioDAO.java
@@ -19,23 +19,56 @@ public class UsuarioDAO {
               PreparedStatement ps = cone.prepareStatement(sql)){
             ps.setString(1,nomeUsuario);
             ps.setString(2, senha);
+            int linhas = ps.executeUpdate();
+            if(linhas >0){
+                System.out.println("verificação concluida: " + linhas);
+            }
             
         }catch(SQLException e){
-            System.out.println("Erro de conexão usuárioDAO, favor verificar");
+            System.out.println("Erro de conexão usuárioDAO, favor verificar: verification error");
         }
     }
     
+    
     //Cadastro do usuário
-    public void insert(){
-         String sql = "INSERT INTO Usuario nome_User =? AND empresa = ? AND cnpj = ? AND nome_Fantasia = ? AND email = ? AND senha = ? AND tipo_empresa =? AND endereco =? AND telefone =? ";
-    }                  //corrigir sintax
+    public void insert(String nomeUsuario, String empresa, String cnpj, String nomeFantasia,String email,String senha,String TipoEmpresa, String endereco, String telefone){
+         String sql = "INSERT INTO Usuario (nome_User,empresa,cnpj,nome_Fantasia,email,senha,tipo_empresa,endereco,telefone,)VALUES(?,?,?,?,?,?,?,?,?)";
+         
+         try(Connection cone = DriverManager.getConnection(conexaoJDBC);
+                 PreparedStatement ps = cone.prepareStatement(sql)){
+             ps.setString(1, nomeUsuario);
+             ps.setString(2, empresa);
+             ps.setString(3, cnpj);
+             ps.setString(4, nomeFantasia);
+             ps.setString(5, email);
+             ps.setString(6, senha);
+             ps.setString(7, TipoEmpresa);
+             ps.setString(8, endereco);
+             ps.setString(9, telefone);
+             
+             int linhas = ps.executeUpdate();
+             if(linhas >0){
+                 System.out.println("cadastro concluido com sucesso! " + linhas+" Linhas");
+             }
+             
+         }catch(SQLException e){
+             System.out.println("Erro de conexão usuárioDAO, favor verificar: insert error");
+         }
+    }                 
     
     
     public void update(){
         
     }
     
-    public void delete(){
+    public void delete(String cnpj){
+        String sql = "DELETE FROM Usuario where cnpj=?";
         
+        try(Connection cone = DriverManager.getConnection(conexaoJDBC);
+             PreparedStatement ps = cone.prepareStatement(sql)){
+            ps.setString(1,cnpj);
+        }catch(SQLException e){
+            System.out.println("Erro de conexão usuárioDAO, favor verificar: insert error");
+        }
     }
 }
